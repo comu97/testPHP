204b1bc8ad6b2b9467d9de0924855ca5695adc0f
comu97 <comu97@gmail.com>
2018-02-06 14:49:45 +0800

    tt

---
+++ testexcel.php
@@ -0,0 +1,27 @@
+<?php 
+
+$dir = "PhpSpreadsheet/";
+$dh  = opendir($dir);
+$dir_list = array($dir);
+while (false !== ($filename = readdir($dh))) {
+	if($filename!="."&&$filename!=".."&&is_dir($dir.$filename))
+		array_push($dir_list, $dir.$filename."/");
+}
+foreach ($dir_list as $dir) {
+	foreach (glob($dir."*.php") as $filename)
+		require_once $filename;
+}
+//require_once __DIR__.'/PhpSpreadsheet/Spreadsheet.php';
+//require_once __DIR__.'/PhpSpreadsheet/Writer/Xlsx'
+
+//use PhpOffice\PhpSpreadsheet\Spreadsheet;
+//use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', 'Hello World !');
+
+$writer = new Xlsx($spreadsheet);
+$writer->save('helloworld.xlsx');
+
+?>
